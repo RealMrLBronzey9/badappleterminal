fc52154cb3987da61257b4b46370328f470d5ef6
Anthony Laforteza <fnafmaster456@gmail.com>
2024-07-01 16:59:31 -0500

    basically done but maybe theres some sort of optimization

--- badapple.c
+++ badapple.c
@@ -1,119 +1,73 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <windows.h>
-#include "cbmp.h"
 #include <string.h>
 
 // To test my c skills, gonna make a bad apple!! terminal
 // Used https://github.com/mattflow/cbmp github project to read the bitmaps
-NBOTE to do!!1: increase resolution and make a luminencense of ascii chars (not just @ and `). Pre render it into strings (separate program and save it into a file) then play the terminal video
+
 // NOTE TO SELF: Bitmaps created 24 fps bad apple!!
-// Bitmaps are 144x108
-#define BITMAP_WIDTH 144
-#define BITMAP_HEIGHT 108
-
+#define FILEPATH_TEXT "D:\\LowLevel\\C Projects\\BadAppleTerminal\\frames.txt"
+#define FRAME_LINES 27  // Each frame has 27 lines
 #define AMT_OF_BITMAPS 5258
+#define LENGTH_OF_LINE 74
 
-// Filepaths for bitmaps
-#define FILEPATH_LINUX  "/mnt/d/LowLevel/C Projects/BadAppleTerminal/Bitmaps/"
-#define FILEPATH_WIN "D:\\LowLevel\\C Projects\\BadAppleTerminal\\Bitmaps\\"
 
-#define BASE_FILEPATH "D:\\LowLevel\\C Projects\\BadAppleTerminal\\Bitmaps\\"
-
-// Possibly 4 by 4 pixels per char ascii
 
 // Functions
 void ClearTerminal();   // Clears terminall
-char GetASCII(int pixelAlpha);  // Gets alpha of pixel and returns an ASCII char
-void PasteFrame();  // Gets the bitmap image file index and then prints the bitmap as ascii 
 
 int main(){
     
-    BMP* bmp;
-    char* filepath;
-    char str_index[10];
-    size_t len;
+    // Add code to open text file and print each frame of bad apple ascii
 
+    // Variables
+    FILE* textFile = NULL;
     char* frame = malloc(3001);
+    char frameLine[80];
 
+    memset(frame, 0 ,3001); // Init buffer to null chars
 
-    for (int i = 1; i < 5258; i++){
+    // Open textfile
+    textFile = fopen(FILEPATH_TEXT, "r");
 
-    // Open bitmap
-    // Append index to the filepath
-    str_index;
-    snprintf(str_index, sizeof(str_index), "%d.bmp", i);    // Make index into a string str_index
-    len = strlen(BASE_FILEPATH) + strlen(str_index) + 1;    // Length of FILEPATH definition and index str and 1 more byte for the \0
-    filepath = malloc(len + 1);   
-    strcpy(filepath, BASE_FILEPATH);   // Append FILEPATH to filepath
-    strcat(filepath, str_index);    // Append str_index to filepath
-
-    // Open the filepath and free the filepath pointer
-    bmp = bopen(filepath);
-    
-    // Paste the frame
-    PasteFrame(bmp, frame);
-    Sleep(13);
-    ClearTerminal();
-
+    // Check if textfile is not read or if mem allocation of frame has failed
+    if(textFile == NULL){
+        printf("ERROR: Text file not read!");
+        getchar();
+        return 1;
+    }
+    else if(frame == NULL){
+        printf("ERROR: frame failed to allocate memory!!");
+        getchar();
+        return 1;
     }
 
-    // deallocate bmp and the filepath
-    bclose(bmp);
-    bmp = NULL;
-    free(filepath);
-    filepath = NULL;
-    free(frame);
-    frame = NULL;
 
-    // Windows terminal needs this or it will immediately pause bruh
-    printf("Program finished, press enter.");
+    // Get frame lines and append to the frame
+    for (int i = 0; i < AMT_OF_BITMAPS - 1; i++){   // Frame number (index)
+        for (int j = 0; j < FRAME_LINES; j++){  // Frame lines
+            fseek(textFile, (LENGTH_OF_LINE*FRAME_LINES*i)+(LENGTH_OF_LINE*j), SEEK_SET);
+            fgets(frameLine, 80, textFile);
+            strncat(frame, frameLine, 81);
+        }
+
+        // Print frame
+        printf("%s", frame);
+        memset(frame, 0 ,3001); // Init buffer to null chars
+        Sleep(50);
+        ClearTerminal();
+
+    }
+    
+
+
+    // Finish program by freeing memories
+    free(frame);
     getchar();
 
     return 0;
 }
-
-// Gets the bitmap image index and prints the bitmap as ascii
-void PasteFrame(BMP* bmp, char* frameBuffer){
-
-    // CREATE ASCII STRING FROM BITMAP IMAGE
-    int pixelAlpha;
-    char pixelASCII;
-    pixelASCII = '\0'; 
-    frameBuffer[0] = '\0';
-    // for looop for the height of bitmap image 
-    // Increment by 4 to check the alpha of every 4 pixels
-    for (int i = BITMAP_HEIGHT-1; i > 0; i -= 8){
-        // for loop for the width of the bitmap image
-        for (int j = 0; j < BITMAP_WIDTH; j += 4){
-            // Get alpha of pixel
-            pixelAlpha = get_pixel_alpha(bmp, j, i);
-            
-            // Get ascii for the pixel
-            pixelASCII = GetASCII(pixelAlpha);
-
-            // Append ascii of pixel to string
-            strncat(frameBuffer, &pixelASCII, 1);
-        }
-        // Add newline after each row
-        char newline[2] = "\n";
-        strncat(frameBuffer, newline, 1);
-    }
-    // Print the bitmap
-    printf("%s", frameBuffer);
-    
-}
-
-// Gets alpha of pixel and returns an ASCII char
-char GetASCII(int pixelAlpha){
-    if (pixelAlpha > 0){
-        return '@';
-    }
-    else{
-        return '`';
-    }
-}
-
 
 // Function to clear terminal 
 void ClearTerminal(){
---
+++ maketxt.c
@@ -0,0 +1,143 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "cbmp.h"
+#include <string.h>
+#include <windows.h>
+
+
+// Separate script to make ascii text file of bad apple!!
+
+// Amount of bitmaps in the bitmap file
+#define AMT_OF_BITMAPS 5258
+#define FILEPATH_WIN "D:\\LowLevel\\C Projects\\BadAppleTerminal\\Bitmaps\\"
+#define FILEPATH_LINUX  "/mnt/d/LowLevel/C Projects/BadAppleTerminal/Bitmaps/"
+
+// Bitmaps are 144x108
+#define BITMAP_WIDTH 144
+#define BITMAP_HEIGHT 108
+
+char GetASCII(int pixelAlpha);
+void PasteFrame(BMP* bmp, char* frameBuffer, FILE* textFile);
+char* GetBitmapFile(char* baseFile, int index);
+
+int main(){
+
+    // Variables
+    FILE* textFile = NULL;   // File path
+    BMP* bmp;   // Bitmap
+    char* bitmapFile = malloc(50);  // Give 50 chars for the 
+
+    // Var to store the ascii string of a frame
+    char* frame = malloc(4001);
+
+    // Create the file to store the ascii
+    textFile = fopen("frames.txt", "w");
+
+    // For every frame in bad apple!!, create a textfile with all bitmaps
+    for (int i = 1; i < AMT_OF_BITMAPS; i++){
+        // Get filepath of bitmap
+        bitmapFile = GetBitmapFile(FILEPATH_WIN, i);
+
+        // Open bitmap
+        bmp = bopen(bitmapFile);
+        PasteFrame(bmp, frame, textFile);
+        printf("Pasting frame: %d\n", i);
+    }
+
+    free(frame);
+    free(bitmapFile);
+    getchar();
+    return 0;
+}
+
+// Gets alpha of pixel and returns an ASCII char
+/*
+    27 Lines y
+
+
+*/
+char GetASCII(int pixelAlpha){
+    if (pixelAlpha > 233){
+        return '@';
+    }
+    else if(pixelAlpha > 212){
+        return '$';
+    }
+    else if(pixelAlpha > 191){
+        return '#';
+    }
+    else if(pixelAlpha > 170){
+        return '*';
+    }
+    else if(pixelAlpha > 149){
+        return '!';
+    }
+    else if(pixelAlpha > 128){
+        return '=';
+    }
+    else if(pixelAlpha > 107){
+        return ';';
+    }
+    else if(pixelAlpha > 86){
+        return ':';
+    }
+    else if(pixelAlpha > 65){
+        return '~';
+    }
+    else if(pixelAlpha > 44){
+        return ',';
+    }
+    else if(pixelAlpha > 23){
+        return '.';
+    }
+    else{
+        return '`';
+    }
+}
+
+
+// Gets the bitmap image index and prints the bitmap as ascii
+void PasteFrame(BMP* bmp, char* frameBuffer, FILE* textFile){
+
+    // CREATE ASCII STRING FROM BITMAP IMAGE
+    int pixelAlpha;
+    char pixelASCII;
+    pixelASCII = '\0'; 
+    frameBuffer[0] = '\0';
+    // for looop for the height of bitmap image 
+    // Increment by 4 to check the alpha of every 4 pixels
+    for (int i = BITMAP_HEIGHT-1; i > 0; i -= 4){
+        // for loop for the width of the bitmap image
+        for (int j = 0; j < BITMAP_WIDTH; j += 2){
+            // Get alpha of pixel
+            pixelAlpha = get_pixel_alpha(bmp, j, i);
+            
+            // Get ascii for the pixel
+            pixelASCII = GetASCII(pixelAlpha);
+
+            // Append ascii of pixel to string
+            strncat(frameBuffer, &pixelASCII, 1);
+        }
+        // Add newline after each row
+        char newline[2] = "\n";
+        strncat(frameBuffer, newline, 1);   
+    }
+    // Store the frame in the textfile
+    fputs(frameBuffer, textFile);
+}
+
+
+char* GetBitmapFile(char* baseFile, int index){
+    char str_index[10];
+    size_t len;
+    char* filepath;
+
+    // Append index of bitmap into the main filepath
+    snprintf(str_index, sizeof(str_index), "%d.bmp", index);    // Make index into a string str_index
+    len = strlen(baseFile) + strlen(str_index) + 1;    // Length of FILEPATH definition and index str and 1 more byte for the \0
+    filepath = malloc(len + 1);   
+    strcpy(filepath, baseFile);   // Append FILEPATH to filepath
+    strcat(filepath, str_index);    // Append str_index to filepath
+    
+    return filepath;
+}
